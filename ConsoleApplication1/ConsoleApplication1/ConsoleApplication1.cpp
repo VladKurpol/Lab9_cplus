52370a9e4b7afcc6520f5e03f31955278e9a9b3e
VladKurpol <48331088+VladKurpol@users.noreply.github.com>
2019-06-06 19:17:10 +0300

    sdasdasd

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,26 +11,23 @@ struct timetable
 {
 	int aud_number;
 	std::string subject, firstname, secondname, lastname, group;
-	struct tm_begin
-	{
-		int tm_sec;     // секунды после минут [0,59]
-		int tm_min;     // минуты после часов [0,59]
-		int tm_hour;    // часы после полуночи [0,23]
-		int tm_mday;    // день месяца [1,31]
-		int tm_mon;     // месяц года (январь = 0) [0,11]
-		int tm_year;    // год (1900 год = 0)
-
+	struct tm_begining {
+		int tm_sec;   // seconds of minutes from 0 to 61
+		int tm_min[2];   // minutes of hour from 0 to 59 =
+		int tm_hour[2];  // hours of day from 0 to 24 =
+		int tm_mday;  // day of month from 1 to 31 =
+		int tm_mon;   // month of year from 0 to 11 =
+		int tm_year;  // year since 1900
 	}b;
-	struct tm_ending
-	{
-		int tm_sec;     // секунды после минут [0,59]
-		int tm_min;     // минуты после часов [0,59]
-		int tm_hour;    // часы после полуночи [0,23]
-		int tm_mday;    // день месяца [1,31]
-		int tm_mon;     // месяц года (январь = 0) [0,11]
-		int tm_year;    // год (1900 год = 0)
-
+	struct tm_ending {
+		int tm_sec;   // seconds of minutes from 0 to 61
+		int tm_min[2];   // minutes of hour from 0 to 59 =
+		int tm_hour[2];  // hours of day from 0 to 24 =
+		int tm_mday;  // day of month from 1 to 31 =
+		int tm_mon;   // month of year from 0 to 11 =
+		int tm_year;  // year since 1900
 	}e;
+	int tm_wday;  // days since sunday
 	std::string enter;
 };
 
@@ -49,19 +46,33 @@ void Input(timetable *table, int number)
 		std::cout << "\n";
 
 		std::cout << "Год: ";
-		std::cin >> table[i].b.tm_mday;
+		std::cin >> table[i].b.tm_year;
 		std::cout << "\n";
 
 		std::cout << "Секунды: ";
-		std::cin >> table[i].b.tm_mday;
+		std::cin >> table[i].b.tm_sec;
 		std::cout << "\n";
 
-		std::cout << "Месяц: ";
-		std::cin >> table[i].b.tm_mon;
+		std::cout << "Минуты: " << "\n";
+		std::cout << "Начало первого экзамена (мин)";
+		std::cin >> table[i].b.tm_min[0];
+		std::cout << "\n";
+		std::cout << "Начало второго экзамена (мин)";
+		std::cin >> table[i].b.tm_min[1];
+		std::cout << "\n";
+		std::cout << "Начало третьего экзамена (мин)";
+		std::cin >> table[i].b.tm_min[2];
 		std::cout << "\n";
 
-		std::cout << "Год: ";
-		std::cin >> table[i].b.tm_mday;
+		std::cout << "Часы: ";
+		std::cout << "Начало первого экзамена (час)";
+		std::cin >> table[i].b.tm_hour[0];
+		std::cout << "\n" << "\n";
+		std::cout << "Начало второго экзамена (час)";
+		std::cin >> table[i].b.tm_hour[0];
+		std::cout << "\n" << "\n";
+		std::cout << "Начало третьего экзамена (час)";
+		std::cin >> table[i].b.tm_hour[0];
 		std::cout << "\n" << "\n";
 
 		std::cout << "Конец экзамена" << "\n";
@@ -75,20 +86,38 @@ void Input(timetable *table, int number)
 		std::cout << "\n";
 
 		std::cout << "Год: ";
-		std::cin >> table[i].e.tm_mday;
+		std::cin >> table[i].e.tm_year;
 		std::cout << "\n";
 
 		std::cout << "Секунды: ";
-		std::cin >> table[i].e.tm_mday;
+		std::cin >> table[i].e.tm_sec;
 		std::cout << "\n";
 
-		std::cout << "Месяц: ";
-		std::cin >> table[i].e.tm_mon;
+		std::cout << "Минуты: " << "\n";
+		std::cout << "Конец первого экзамена (мин)";
+		std::cin >> table[i].e.tm_min[0];
+		std::cout << "\n";
+		std::cout << "Конец второго экзамена (мин)";
+		std::cin >> table[i].e.tm_min[1];
+		std::cout << "\n";
+		std::cout << "Конец третьего экзамена (мин)";
+		std::cin >> table[i].e.tm_min[2];
 		std::cout << "\n";
 
-		std::cout << "Год: ";
-		std::cin >> table[i].e.tm_mday;
+		std::cout << "Часы: ";
+		std::cout << "Конец первого экзамена (час)";
+		std::cin >> table[i].e.tm_hour[0];
 		std::cout << "\n" << "\n";
+		std::cout << "Конец второго экзамена (час)";
+		std::cin >> table[i].e.tm_hour[0];
+		std::cout << "\n" << "\n";
+		std::cout << "Конец третьего экзамена (час)";
+		std::cin >> table[i].e.tm_hour[0];
+		std::cout << "\n" << "\n";
+
+		std::cout << "День недели (1-7)" << "\n";
+		std::cin >> table[i].tm_wday;
+		std::cout << "\n";
 
 		std::cout << "Предмет: ";
 		std::cin >> table[i].subject;
@@ -111,6 +140,10 @@ void Input(timetable *table, int number)
 		std::cout << "Группа: ";
 		std::cin >> table[i].group;
 		std::cout << "\n" << "\n";
+
+		std::cout << "Аудитория: ";
+		std::cin >> table[i].aud_number;
+		std::cout << "\n" << "\n";
 	}
 }
 
@@ -125,63 +158,286 @@ void Save_in(timetable *table, int number)
 	{
 		database << table[i].b.tm_mday;
 		database << table[i].b.tm_mon;
-		database << table[i].b.tm_mday;
-		database << table[i].b.tm_mday;
-		database << table[i].b.tm_mon;
-		database << table[i].b.tm_mday;
+		database << table[i].b.tm_year;
+		database << table[i].b.tm_sec;
+		database << table[i].b.tm_min[0];
+		database << table[i].b.tm_min[1];
+		database << table[i].b.tm_min[2];
+		database << table[i].b.tm_hour[0];
+		database << table[i].b.tm_hour[1];
+		database << table[i].b.tm_hour[2];
 		database << table[i].e.tm_mday;
 		database << table[i].e.tm_mon;
-		database << table[i].e.tm_mday;
-		database << table[i].e.tm_mday;
-		database << table[i].e.tm_mon;
-		database << table[i].e.tm_mday;
+		database << table[i].e.tm_year;
+		database << table[i].e.tm_sec;
+		database << table[i].e.tm_min[0];
+		database << table[i].e.tm_min[1];
+		database << table[i].e.tm_min[2];
+		database << table[i].e.tm_hour[0];
+		database << table[i].e.tm_hour[1];
+		database << table[i].e.tm_hour[2];
+		database << table[i].tm_wday;
 		database << table[i].subject;
 		database << table[i].lastname;
 		database << table[i].firstname;
 		database << table[i].secondname;
 		database << table[i].group;
+		database << table[i].aud_number;
 	}
+	system("cls");
 	std::cout << "Успешная запись!" << "\n";
-
+	menu(table, number);
 }
 
 void Find_inf(timetable *table, int number)
 {
-	int sum = 0;
+	std::cout << "Введите дату, по которой вы хотите найти экзамен: (ДДММ) " << "\n";
+	
+	int tm_mday = 0;
+	int tm_mon = 0;
+
+	std::cin >> tm_mday;
+	std::cin >> tm_mon;
+	int pointer = 0;
+	int total_time = 0;
+
 	for (size_t i = 0; i < number; i++)
 	{
-		if (table[i].subject == "informatic")
+		if (tm_mday == table[i].b.tm_mday && tm_mon == table[i].b.tm_mon)
 		{
-			std::cout << table[i].subject << "\n";
-			std::cout << "Дата:" << table[i].b.tm_mday << table[i].b.tm_mon << table[i].b.tm_year << "   " << table[i].b.tm_hour << table[i].b.tm_min << table[i].b.tm_sec;
-			sum +=
+			for (size_t j = 0; j < 2; j++)
+			{
+				if (table[i].subject == "Информатика")
+				{
+					pointer++;
+					total_time += (table[i].e.tm_hour[j] - table[i].b.tm_hour[j]) * 60 + (table[i].e.tm_min[j] - table[i].b.tm_min[j]);
+				}
+			}	
 		}
+	}
+
+	std::cout << "Количество экзаменов по информатике: " << pointer << "\n";
+	std::cout << "Общее время длительности экзаменов: " << total_time << "\n";
+	menu(table, number);
+}
+
+void Find_duration(timetable *table, int number)
+{
+	int max = table[0].e.tm_hour[0];
+	int hours = 0;
+	int mins = 0;
+
+	for (size_t i = 0; i < number; i++)
+	{
+		for (size_t j = 0; j < 2; j++)
+		{
+			if (table[i].e.tm_hour[j] > max)
+			{
+				hours = table[i].e.tm_hour[j];
+				mins = table[i].e.tm_min[j];
+			}
+		}
+	}
+
+	std::cout << "Самое позднее время окончания экзамена (ЧЧММ) " << hours << mins << "\n";
+	menu(table, number);
+}
+
+void Save_new(timetable *table, int number, std::string file_name)
+{
+	std::cout << "Сохранение в файл " << file_name << " !" << "\n";
+
+	std::ofstream database;
+	database.open(file_name, std::ios::app);
+	for (size_t i = 0; i < number; i++)
+	{
+		database << table[i].b.tm_mday;
+		database << table[i].b.tm_mon;
+		database << table[i].b.tm_year << " ";
+		database << table[i].b.tm_sec;
+		database << table[i].b.tm_min[0];
+		database << table[i].b.tm_hour[0] << " ";
+		database << table[i].b.tm_min[1];
+		database << table[i].b.tm_hour[1] << " ";
+		database << table[i].b.tm_min[2];
+		database << table[i].b.tm_hour[2] << " ";
+		database << table[i].e.tm_mday;
+		database << table[i].e.tm_mon;
+		database << table[i].e.tm_year << " ";
+		database << table[i].e.tm_sec;
+		database << table[i].e.tm_min[0];
+		database << table[i].e.tm_hour[0] << " ";
+		database << table[i].e.tm_min[1];
+		database << table[i].e.tm_hour[1] << " ";
+		database << table[i].e.tm_min[2];	
+		database << table[i].e.tm_hour[2] << " ";
+		database << table[i].tm_wday << " ";
+		database << table[i].subject << " ";
+		database << table[i].lastname << " ";
+		database << table[i].firstname << " ";
+		database << table[i].secondname << " ";
+		database << table[i].group << " ";
+		database << table[i].aud_number << " ";
+		database << "====================" << " ";
+	}
+	std::cout << "Успешная запись!" << "\n";
+}
+
+void Save_in_diff(timetable *table, int number)
+{
+	std::cout << "Сохранение в файлы по дням недели!" << "\n";
+	for (size_t i = 0; i < number; i++)
+	{
+		if (table[i].tm_wday == 1)
+		{
+			std::string file_name = "1";
+			Save_new(table, number, file_name);
+		}
+		else if (table[i].tm_wday == 2)
+		{
+			std::string file_name = "2";
+			Save_new(table, number, file_name);
+		}
+		else if (table[i].tm_wday == 3)
+		{
+			std::string file_name = "3";
+			Save_new(table, number, file_name);
+		}
+		else if (table[i].tm_wday == 4)
+		{
+			std::string file_name = "4";
+			Save_new(table, number, file_name);
+		}
+		else if (table[i].tm_wday == 5)
+		{
+			std::string file_name = "5";
+			Save_new(table, number, file_name);
+		}
+		else if (table[i].tm_wday == 6)
+		{
+			std::string file_name = "6";
+			Save_new(table, number, file_name);
+		}
+		else if (table[i].tm_wday == 7)
+		{
+			std::string file_name = "7";
+			Save_new(table, number, file_name);
+		}
+	}
+	std::cout << "Успешное создание новых файлов!" << "\n";
+	menu(table, number);
+}
+
+void Read(timetable *table, int number)
+{
+	std::cout << "Считывание из файла" << "\n";
+	std::cout << "Выберете имя файла!" << "\n";
+	std::string file_name;
+	std::cin >> file_name;
+	
+	std::ifstream database;
+	database.open(file_name);
+	for (size_t i = 0; i < number; i++)
+	{
+		database >> table[i].b.tm_mday;
+		database >> table[i].b.tm_mon;
+		database >> table[i].b.tm_year;
+		database >> table[i].b.tm_sec;
+		database >> table[i].b.tm_min[0];
+		database >> table[i].b.tm_hour[0];
+		database >> table[i].b.tm_min[1];
+		database >> table[i].b.tm_hour[1];
+		database >> table[i].b.tm_min[2];
+		database >> table[i].b.tm_hour[2];
+		database >> table[i].e.tm_mday;
+		database >> table[i].e.tm_mon;
+		database >> table[i].e.tm_year;
+		database >> table[i].e.tm_sec;
+		database >> table[i].e.tm_min[0];
+		database >> table[i].e.tm_hour[0];
+		database >> table[i].e.tm_min[1];
+		database >> table[i].e.tm_hour[1];
+		database >> table[i].e.tm_min[2];
+		database >> table[i].e.tm_hour[2];
+		database >> table[i].tm_wday;
+		database >> table[i].subject;
+		database >> table[i].lastname;
+		database >> table[i].firstname;
+		database >> table[i].secondname;
+		database >> table[i].group;
+		database >> table[i].aud_number;
+		database >> table[i].enter;
+	}
+	std::cout << "Успешное считывание!" << "\n";
+	menu(table, number);
+}
+
+void menu(timetable *table, int number)
+{
+pointer1:
+	std::cout << "Сделайте выбор" << "\n";
+	std::cout << "1. Ввести информацию" << "\n" << "2. Считать информацию" << "\n";
+	int choice1 = 0;
+	std::cin >> choice1;
+	if (choice1 == 1)
+	{
+		system("cls");
+		std::cout << "Введите данные: " << "\n" << "\n";
+		Input(table, number);
+		Save_in(table, number);
+	}
+	else if (choice1 == 2)
+	{
+		system("cls");
+		Read(table, number);
+	}
+	else
+	{
+		system("cls");
+		std::cout << "Некорректный ввод!" << "\n";
+		goto pointer1;
+	}
+
+
+pointer:
+	int choice = 0;
+	std::cout << "1. Найти число экзаменов по информатике и общую длительность." << "\n" << "2. Найти самое позднее время окончания экзамена (ЧЧММ)" << "\n" << "3. Распределить данные по файлам." << "\n";
+	std::cin >> choice;
+	if (choice == 1)
+	{
+		system("cls");
+		Find_inf(table, number);
+	}
+	else if (choice == 2)
+	{
+		system("cls");
+		Find_duration(table, number);
+	}
+	else if (choice == 3)
+	{
+		system("cls");
+		Save_in_diff(table, number);
+	}
+	else
+	{
+		system("cls");
+		std::cout << "Некорректный ввод!" << "\n";
+		goto pointer;
 	}
 }
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
 	std::cout << "Сколько записей вы хотите записать в файл?" << "\n";
 
 	int number = 0;
 
 	std::cin >> number;
-	std::cout << "Введите данные: " << "\n" << "\n";
-
 	timetable *table = new timetable[number];
-
-	Input(table, number);
-	Save_in(table, number);
-	Find_inf(table, number);
 }
+	
+	
+	
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
